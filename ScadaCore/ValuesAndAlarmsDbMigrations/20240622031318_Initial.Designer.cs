ef7bb49ca0c695ee781648f44e16e04aec7e8737
darkokos <darkosvilar02@gmail.com>
2024-06-23 17:32:50 +0200

    feat: user repository

--- ScadaCore/ValuesAndAlarmsDbMigrations/20240622031318_Initial.Designer.cs
+++ ScadaCore/ValuesAndAlarmsDbMigrations/20240622031318_Initial.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ScadaCore.Repositories;
+using ScadaCore.Data;
 
 #nullable disable
 
