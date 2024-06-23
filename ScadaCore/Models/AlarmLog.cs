5a2189fce684c48f166195faa122f19f39fdee7a
darkokos <darkosvilar02@gmail.com>
2024-06-24 01:16:18 +0200

    feat: alarm log xml repository

--- ScadaCore/Models/AlarmLog.cs
+++ ScadaCore/Models/AlarmLog.cs
@@ -1,8 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Xml.Linq;
+using Lombok.NET;
 
 namespace ScadaCore.Models;
 
-public class AlarmLog {
+[NoArgsConstructor]
+public partial class AlarmLog {
     [Key] public int Id { get; set; }
     [Required] public int AlarmId { get; set; }
     [Required] public AlarmType Type { get; set; }
@@ -12,4 +16,22 @@ public class AlarmLog {
     [StringLength(10, MinimumLength = 1, ErrorMessage = "{0} must be between {2} and {1} characters long.")]
     public string ValueName { get; set; }
     [Required] public DateTime Timestamp { get; set; }
+
+    public AlarmLog(XElement alarmLogXElement) {
+        Id = int.TryParse(alarmLogXElement.Value, out var value) ? value : -1;
+        AlarmId = int.TryParse(alarmLogXElement.Attribute("alarmId")?.Value, out var alarmId) ? alarmId : -1;
+        Type = Enum.TryParse(alarmLogXElement.Attribute("type")?.Value, out AlarmType type) ? type : AlarmType.High;
+        ValueName = alarmLogXElement.Attribute("valueName")?.Value ?? "";
+        Timestamp =
+            DateTime
+                .TryParseExact(
+                    alarmLogXElement.Attribute("timestamp")?.Value,
+                    "dd/MM/yyyy",
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.None,
+                    out var timestamp
+                )
+                ? timestamp
+                : default;
+    }
 }
