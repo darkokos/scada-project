936e27748bdc737f7685d49b7bdbf4c8592fcb01
darkokos <darkosvilar02@gmail.com>
2024-06-23 18:34:41 +0200

    feat: tag log repository

---
+++ ScadaCore/Repositories/ITagLogRepository.cs
@@ -0,0 +1,9 @@
+﻿using ScadaCore.Models;
+
+namespace ScadaCore.Repositories;
+
+public interface ITagLogRepository {
+    Task<TagLog?> GetTagLogAsync(int id);
+    
+    Task<TagLog?> CreateTagLogAsync(TagLog tagLog);
+}
---
+++ ScadaCore/Repositories/TagLogRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScadaCore.Data;
+using ScadaCore.Models;
+
+namespace ScadaCore.Repositories;
+
+public class TagLogRepository(ValueAndAlarmContext valueAndAlarmContext) : ITagLogRepository {
+    public async Task<TagLog?> GetTagLogAsync(int id) {
+        return await valueAndAlarmContext.TagLogs.FirstOrDefaultAsync(tagLog => tagLog.Id == id);
+    }
+    
+    public async Task<TagLog?> CreateTagLogAsync(TagLog tagLog) {
+        var savedTagLog = await valueAndAlarmContext.TagLogs.AddAsync(tagLog);
+        return await valueAndAlarmContext.SaveChangesAsync() >= 0 ? savedTagLog.Entity : null;
+    }
+}
