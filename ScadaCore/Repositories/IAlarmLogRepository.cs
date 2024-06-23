86c86dcf29868c16675f3828da88a9633071ae7f
darkokos <darkosvilar02@gmail.com>
2024-06-23 18:44:44 +0200

    feat: alarm log db repository

---
+++ ScadaCore/Repositories/IAlarmLogRepository.cs
@@ -0,0 +1,9 @@
+﻿using ScadaCore.Models;
+
+namespace ScadaCore.Repositories;
+
+public interface IAlarmLogRepository {
+    Task<AlarmLog?> GetAlarmLogAsync(int id);
+    
+    Task<AlarmLog?> CreateAlarmLogAsync(AlarmLog alarmLog);
+}
