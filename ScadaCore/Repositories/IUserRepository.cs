f67ac20855e86a3785a91c8af0c13eaa709136a4
darkokos <darkosvilar02@gmail.com>
2024-06-23 18:22:32 +0200

    fix: logs model fix

--- ScadaCore/Repositories/IUserRepository.cs
+++ ScadaCore/Repositories/IUserRepository.cs
@@ -3,7 +3,7 @@
 namespace ScadaCore.Repositories;
 
 public interface IUserRepository {
-    Task<User?> GetUserByUsernameAsync(string username);
+    Task<User?> GetUserAsync(string username);
     
     Task<User?> CreateUserAsync(User user);
 }
