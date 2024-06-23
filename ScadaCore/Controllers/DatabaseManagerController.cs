339409b9b48e634ba867ba5cda95c3b14fe71b74
Dragoslav Tamindzija <dragodev@protonmail.com>
2024-06-23 14:08:23 +0200

    feat: init database manager controller

---
+++ ScadaCore/Controllers/DatabaseManagerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScadaCore.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DatabaseManagerController : ControllerBase
+{
+    private readonly ILogger<DatabaseManagerController> _logger;
+
+    public DatabaseManagerController(ILogger<DatabaseManagerController> logger)
+    {
+        _logger = logger;
+    }
+    
+    
+}
