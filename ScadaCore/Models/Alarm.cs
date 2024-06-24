24764f0e77087de5b318c0d0b71ac579b83fec0c
darkokos <darkosvilar02@gmail.com>
2024-06-24 19:30:19 +0200

    feat: alarm repository

--- ScadaCore/Models/Alarm.cs
+++ ScadaCore/Models/Alarm.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using Lombok.NET;
 
 namespace ScadaCore.Models;
 
@@ -13,8 +15,9 @@ public enum AlarmPriority {
     High
 }
 
-public class Alarm {
-    [Key] public int Id { get; set; }
+[NoArgsConstructor]
+public partial class Alarm {
+    public int Id { get; set; }
     [Required] public AlarmType Type { get; set; } 
     [Required] public AlarmPriority Priority { get; set; }
     
@@ -24,4 +27,41 @@ public class Alarm {
     [Required]
     [StringLength(10, MinimumLength = 1, ErrorMessage = "{0} must be between {2} and {1} characters long.")]
     public string ValueName { get; set; }
+
+    public static string GetXName() {
+        return "alarm";
+    }
+    
+    public static string GetTypeXAttributeName() {
+        return "type";
+    }
+    
+    public static string GetPriorityXAttributeName() {
+        return "priority";
+    }
+    
+    public static string GetThresholdXAttributeName() {
+        return "threshold";
+    }
+    
+    public static string GetValueNameXAttributeName() {
+        return "valueName";
+    }
+
+    public Alarm(XElement alarmXElement) {
+        Id = int.TryParse(alarmXElement.Value, out var id) ? id : -1;
+        Type =
+            Enum.TryParse(alarmXElement.Attribute(GetTypeXAttributeName())?.Value, out AlarmType alarmType)
+                ? alarmType
+                : default;
+        Priority = Enum.TryParse(
+            alarmXElement.Attribute(GetPriorityXAttributeName())?.Value,
+            out AlarmPriority alarmPriority
+        ) ? alarmPriority : default;
+        Threshold =
+            int.TryParse(alarmXElement.Attribute(GetThresholdXAttributeName())?.Value, out var threshold)
+                ? threshold
+                : -1;
+        ValueName = alarmXElement.Attribute(GetValueNameXAttributeName())?.Value ?? "";
+    }
 }
