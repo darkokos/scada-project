32ab860fa7b8c2cee41e314a9c98b02119f34bff
darkokos <darkosvilar02@gmail.com>
2024-06-28 18:34:06 +0200

    feat: rtu controller endpoints

---
+++ ScadaCore/Controllers/RtuController.cs
@@ -0,0 +1,43 @@
+﻿using Common.RealTimeUnit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScadaCore.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RtuController : ControllerBase {
+    [HttpGet("{tagName}")]
+    public async Task<IActionResult> GetTag(string tagName) {
+        return StatusCode(StatusCodes.Status501NotImplemented);
+    }
+    
+    [HttpGet("analog/input/{tagName}")]
+    public async Task<IActionResult> GetAnalogInputUnitInformation(string tagName) {
+        return StatusCode(StatusCodes.Status501NotImplemented);
+    }
+    
+    [HttpGet("analog/output/{tagName}")]
+    public async Task<IActionResult> GetAnalogOutputUnitInformation(string tagName) {
+        return StatusCode(StatusCodes.Status501NotImplemented);
+    }
+    
+    [HttpGet("digital/input/{tagName}")]
+    public async Task<IActionResult> GetDigitalInputUnitInformation(string tagName) {
+        return StatusCode(StatusCodes.Status501NotImplemented);
+    }
+    
+    [HttpGet("digital/output/{tagName}")]
+    public async Task<IActionResult> GetDigitalOutputUnitInformation(string tagName) {
+        return StatusCode(StatusCodes.Status501NotImplemented);
+    }
+    
+    [HttpPost("analog")]
+    public async Task<IActionResult> SendAnalogValue([FromBody] AnalogValueDto dto) {
+        return StatusCode(StatusCodes.Status501NotImplemented);
+    }
+    
+    [HttpPost("digital")]
+    public async Task<IActionResult> SendDigitalValue([FromBody] DigitalValueDto dto) {
+        return StatusCode(StatusCodes.Status501NotImplemented);
+    }
+}
