9169083ce067dec503401501c2268052e0d41bcf
darkokos <darkosvilar02@gmail.com>
2024-06-30 18:51:30 +0200

    feat: digital simulation driver

---
+++ ScadaCore/Services/TagProcessing.cs
@@ -0,0 +1,6 @@
+﻿namespace ScadaCore.Services;
+
+public class TagProcessing(ITagService tagService) : BackgroundService {
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken) {
+    }
+}
