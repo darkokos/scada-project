6d7601ca2a07eea28f3a4f793666cef42697f0f4
teodord25 <djuric.teodor25@gmail.com>
2024-06-30 14:54:42 +0200

    minor? changes to alarmlogdbrepo

--- ScadaCore/Repositories/AlarmLogDbRepository.cs
+++ ScadaCore/Repositories/AlarmLogDbRepository.cs
@@ -1,15 +1,30 @@
-﻿using ScadaCore.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ScadaCore.Data;
 using ScadaCore.Models;
 
 namespace ScadaCore.Repositories;
+public class AlarmLogDbRepository : IAlarmLogRepository
+{
+    private readonly ValueAndAlarmContext _valueAndAlarmContext;
 
-public class AlarmLogDbRepository(ValueAndAlarmContext valueAndAlarmContext) : IAlarmLogRepository {
-    public async Task<AlarmLog?> GetAlarmLogAsync(int id) {
-        return await valueAndAlarmContext.AlarmLogs.FindAsync(id);
+    public AlarmLogDbRepository(ValueAndAlarmContext valueAndAlarmContext)
+    {
+        _valueAndAlarmContext = valueAndAlarmContext;
     }
 
-    public async Task<AlarmLog?> CreateAlarmLogAsync(AlarmLog alarmLog) {
-        var savedAlarmLog = await valueAndAlarmContext.AlarmLogs.AddAsync(alarmLog);
-        return await valueAndAlarmContext.SaveChangesAsync() >= 0 ? savedAlarmLog.Entity : null;
+    public async Task<AlarmLog?> GetAlarmLogAsync(int id)
+    {
+        return await _valueAndAlarmContext.AlarmLogs.FindAsync(id);
+    }
+
+    public async Task<AlarmLog?> CreateAlarmLogAsync(AlarmLog alarmLog)
+    {
+        var savedAlarmLog = await _valueAndAlarmContext.AlarmLogs.AddAsync(alarmLog);
+        await _valueAndAlarmContext.SaveChangesAsync();
+        return savedAlarmLog.Entity;
+    }
+    public async Task<IEnumerable<AlarmLog>> GetAllAlarmLogsAsync()
+    {
+        return await _valueAndAlarmContext.AlarmLogs.ToListAsync();
     }
 }
