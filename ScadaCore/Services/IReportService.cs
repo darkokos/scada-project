39546ce323983418ced158464e934af1cbc3195d
teodord25 <djuric.teodor25@gmail.com>
2024-06-30 14:55:04 +0200

    impl reportservice interface

---
+++ ScadaCore/Services/IReportService.cs
@@ -0,0 +1,18 @@
+﻿using ScadaCore.Models;
+
+namespace ScadaCore.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IReportService
+{
+        Task<IEnumerable<AlarmLog>> GetAlarmsInSpecificTimePeriod(DateTime startTime, DateTime endTime);
+        Task<IEnumerable<AlarmLog>> GetAlarmsOfSpecificPriority(AlarmPriority priority);
+        Task<IEnumerable<TagLog>> GetTagLogsInSpecificTimePeriod(DateTime startTime, DateTime endTime);
+        Task<IEnumerable<TagLog>> GetAllLogsForSpecificTag(string tagName);
+        
+        Task<IEnumerable<TagLog>> GetLastLogOfAllAITags();
+        Task<IEnumerable<TagLog>> GetLastLogOfAllDITags();
+}
