eaf58f5ab48efd95b5e86e1d18043fc620b70ba1
teodord25 <djuric.teodor25@gmail.com>
2024-06-30 14:50:46 +0200

    add classes to common

---
+++ Common/ReportManagerCommon/TagLog.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.ReportManagerCommon;
+
+public class TagLog {
+    public int Id { get; set; }
+    public string TagName { get; set; }
+    public DateTime Timestamp { get; set; }
+}
